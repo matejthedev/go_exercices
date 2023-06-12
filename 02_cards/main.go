09bcbef48d1d8c66bc3b439c2a9e4db3dfbd32ac
Matej Pavic <mpavic.dev@gmail.com>
2023-06-12 09:14:00 +0200

    slices and for loops

--- 02_cards/main.go
+++ 02_cards/main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello from the cards app")
+	cards := []string{"Ace of spades", newCard(), "Six of spades"}
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "King of diamonds"
 }
